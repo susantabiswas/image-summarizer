955006ff38165e18d36e923219c018dedb82b16d
susantabiswas <susantasky@gmail.com>
2018-07-02 20:11:15 +0530

    file for preprocessing images

---
+++ preprocessing_images.py
@@ -0,0 +1,32 @@
+from keras.applications.inception_v3 import preprocess_input
+from keras.preprocessing.image import load_img, img_to_array
+import pickle 
+import numpy as np
+import keras
+from keras.applications import InceptionV3
+
+# load the pretrained inception model in keras
+image_model = InceptionV3(include_top=False)
+#image_model.add(L.GlobalAveragePooling2D())
+image_model = keras.engine.training.Model(image_model.inputs, keras.layers.GlobalAveragePooling2D()(image_model.output))
+
+# location for extracted images
+files_val = os.listdir('train2014/')
+
+images = []
+for filename in files_val:
+    # read image
+    img = load_img('train2014/'+filename, target_size=(229, 229))
+    # convert to numpy array
+    img = img_to_array(img)
+    images.append(img)
+    
+batch_img = np.array(images)
+
+# preprocess image
+img = preprocess_input(batch_img)
+# get the model predictions
+pred = image_model.predict(batch_img)
+
+with open('drive/Colab Notebooks/train_img_embeds.pickle', 'wb') as f:
+    pickle.dump(pred, f)
